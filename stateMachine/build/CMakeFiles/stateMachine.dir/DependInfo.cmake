4a2de2be1903d244d9ad5519283b0bd4593b7479
ahmedtawfiiiq <ahmedtawfiiiq24@gmail.com>
2022-09-21 20:10:26 +0200

    update

---
+++ stateMachine/build/CMakeFiles/stateMachine.dir/DependInfo.cmake
@@ -0,0 +1,22 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "C:/Users/Ahmad/OneDrive/Desktop/trial6/Ethernet_Frame/stateMachine/configurationHandler.cpp" "CMakeFiles/stateMachine.dir/configurationHandler.cpp.obj" "gcc" "CMakeFiles/stateMachine.dir/configurationHandler.cpp.obj.d"
+  "C:/Users/Ahmad/OneDrive/Desktop/trial6/Ethernet_Frame/stateMachine/frameMachine.cpp" "CMakeFiles/stateMachine.dir/frameMachine.cpp.obj" "gcc" "CMakeFiles/stateMachine.dir/frameMachine.cpp.obj.d"
+  "C:/Users/Ahmad/OneDrive/Desktop/trial6/Ethernet_Frame/stateMachine/frameStates.cpp" "CMakeFiles/stateMachine.dir/frameStates.cpp.obj" "gcc" "CMakeFiles/stateMachine.dir/frameStates.cpp.obj.d"
+  "C:/Users/Ahmad/OneDrive/Desktop/trial6/Ethernet_Frame/stateMachine/main.cpp" "CMakeFiles/stateMachine.dir/main.cpp.obj" "gcc" "CMakeFiles/stateMachine.dir/main.cpp.obj.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ stateMachine/build/CMakeFiles/stateMachine.dir/cmake_clean.cmake
@@ -0,0 +1,19 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/stateMachine.dir/configurationHandler.cpp.obj"
+  "CMakeFiles/stateMachine.dir/configurationHandler.cpp.obj.d"
+  "CMakeFiles/stateMachine.dir/frameMachine.cpp.obj"
+  "CMakeFiles/stateMachine.dir/frameMachine.cpp.obj.d"
+  "CMakeFiles/stateMachine.dir/frameStates.cpp.obj"
+  "CMakeFiles/stateMachine.dir/frameStates.cpp.obj.d"
+  "CMakeFiles/stateMachine.dir/main.cpp.obj"
+  "CMakeFiles/stateMachine.dir/main.cpp.obj.d"
+  "libstateMachine.dll.a"
+  "stateMachine.exe"
+  "stateMachine.exe.manifest"
+  "stateMachine.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/stateMachine.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
